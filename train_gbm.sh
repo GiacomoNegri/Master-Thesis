e4d1458d8841ee61c0a3562362d6a1ae872a580c
GiacomoNegri <negrigiacomo02@gmail.com>
2026-03-20 12:07:41 +0100

    Update the files

---
+++ train_gbm.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+#SBATCH --job-name=gbm_csdi
+#SBATCH --account=3155287
+#SBATCH --partition=stud
+#SBATCH --gpus=1
+#SBATCH --nodelist=gnode04
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=32G
+#SBATCH --time=20:00:00
+#SBATCH --output=out/%x_%j.out
+#SBATCH --error=err/%x_%j.err
+#SBATCH --mail-type=END
+#SBATCH --mail-user=3155287@studbocconi.it
+
+set -x
+mkdir -p out err
+
+echo "Job running on:"
+hostname
+whoami
+
+module load miniconda3
+source /software/miniconda3/etc/profile.d/conda.sh
+conda activate thesis
+
+echo "Running on:"
+hostname
+nvidia-smi || True
+
+echo "Python executable:"
+which python
+python --version
+
+python -c "import torch; print(torch.__version__); print(torch.version.cuda); print(torch.cuda.is_available()); print(torch.cuda.device_count())"
+
+cd /home/3155287/Thesis/Master-Thesis
+
+
+echo "PWD:"
+pwd
+
+srun python -u csdi_train.py --config configs/csdi_gbm.yaml --epochs 5 --data_root ./data/fake_individual_gbm
+echo "Python exit code: $?"
+
+echo "Job finished."
+
