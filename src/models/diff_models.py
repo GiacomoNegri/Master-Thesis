f736357721b0f70596520e7a1ccb67258c5c72e0
GiacomoNegri <negrigiacomo02@gmail.com>
2026-04-11 09:04:54 +0200

    weight initialization changed to normal (0,0.01)

--- src/models/diff_models.py
+++ src/models/diff_models.py
@@ -76,7 +76,10 @@ class diff_CSDI(nn.Module):
         self.input_projection = Conv1d_with_init(inputdim, self.channels, 1)
         self.output_projection1 = Conv1d_with_init(self.channels, self.channels, 1)
         self.output_projection2 = Conv1d_with_init(self.channels, 1, 1)
+        # 0-initialization as in the papaer
         nn.init.zeros_(self.output_projection2.weight)
+        # Normal initalization
+        nn.init.normal_(self.output_projection2.bias, mean=0.0, std=0.01)
 
         # Stack of residual layers
         self.residual_layers = nn.ModuleList(
