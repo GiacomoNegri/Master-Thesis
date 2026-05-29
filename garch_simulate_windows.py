44f1d78a6d95f656e887c4d99f0176b8ae4ebca6
GiacomoNegri <negrigiacomo02@gmail.com>
2026-05-29 15:20:41 +0200

    mod

--- garch_simulate_windows.py
+++ garch_simulate_windows.py
@@ -24,6 +24,7 @@ Usage:
         --num_samples 250 \
         [--seed 42]
 python garch_simulate_windows.py --input_folder ./data/SNP500_individual_processed --garch_dir ./checkpoints/garch --out_dir ./data/generated/garch --seq_len 512 --stride 100 --num_samples 10 --seed 42
+python garch_simulate_windows.py --input_folder ./data/SNP500_individual_processed_replication --garch_dir ./checkpoints/garch_replication --out_dir ./data/generated/garch_replication --seq_len 512 --stride 100 --num_samples 10 --seed 42
 """
 
 import argparse
